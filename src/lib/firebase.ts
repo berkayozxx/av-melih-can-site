48ef2c428c80ac17f9400fe59a4af85788445c58
Berka <berka@example.com>
2025-12-29 02:09:48 +0300

    Update Firebase Config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,18 +3,18 @@ import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For technical reasons, these should ideally be in .env.local, but for this setup we will keep them here for now
-// YOU MUST REPLACE THESE VALUES WITH YOUR OWN FIREBASE PROJECT CONFIG
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyBd3MzwjcawvH9iiCJxOJ3GvfZc8NixO5E",
+    authDomain: "melih-can-hukuk.firebaseapp.com",
+    projectId: "melih-can-hukuk",
+    storageBucket: "melih-can-hukuk.firebasestorage.app",
+    messagingSenderId: "356237890728",
+    appId: "1:356237890728:web:6c106c3c7463210e6d5e9d",
+    measurementId: "G-LHXV1QJHCP"
 };
 
 // Initialize Firebase
+// Using getApps() prevents re-initialization error in Next.js hot-reload / serverless
 let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
 
